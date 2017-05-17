0acc1bd31a06fe8a2b3b32011fa9ecf75fafbd67
Binh <technobia@gmail.com>
2017-05-17 23:33:00 +0700

    update git ignore and readme

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ var path = require('path');
 module.exports = {
 	entry: './main.jsx',
 	output: {
-        path: path.resolve(__dirname, 'dist'),
-		publicPath: '/dist/',
+        path: path.resolve(__dirname, 'build'),
+		publicPath: '/build/',
 		filename: 'index.js'
 	},
     devServer: {
